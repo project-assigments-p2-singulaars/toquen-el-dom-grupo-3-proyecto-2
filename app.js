0113b8fdbef8ff3bcc272ea056198aee049d8f88
Daniel Cueto <dcueto14.info@gmail.com>
2024-03-22 12:06:02 +0100

    fix: polished final changes sprint 1

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-import { KeyboardKey } from "./src/components/Keyboard/Key/keyboard-key.js";
-import { KeyboardOctave } from "./src/components/Keyboard/Octave/keyboard-octave.js";
-
 document.addEventListener('DOMContentLoaded', initApp);
 
 function initApp(){
